1ce2f99250ac37c6a0ae7eeefe8e20c5b6082774
rodrigopv <lrpena@miuandes.cl>
2021-05-16 23:57:50 -0400

    feat(grabbers): add celery job for grab and ingesting datasets

--- backend/app/worker.py
+++ backend/app/worker.py
@@ -1,18 +1,17 @@
 from celery import Celery
 from celery.schedules import crontab
+from grabber_run import grab_new_datasets
+from ingester_run import ingest_pending_snapshots
 
 app = Celery('worker', backend='redis://redis:6379', broker='redis://redis:6379/0')
 app.conf.update(CELERY_REDIS_SCHEDULER_URL = 'redis://redis:6379')
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
-    # Calls test('hello') every 10 seconds.
-    sender.add_periodic_task(3600.0, test.s('alive check'), name='add every 3600')
-
-    sender.add_periodic_task(
-        crontab(hour=6, minute=30),
-        test.s('TODO: execute daily PCH grabber here'),
-    )
+  return
 
 @app.task
-def test(arg):
-    print(arg)
+def grab_and_ingest(collector_name):
+    print(f"Starting grabber for collector {collector_name}")
+    grab_new_datasets(collector_name)
+    print(f"Starting ingestion process for collector {collector_name}")
+    ingest_pending_snapshots(collector_name)
